0c2e59392c024b72af6ffe375c1b9f8954b66f73
Alexandre <alexandreeduardolemes@gmail.com>
2023-01-11 02:43:57 -0300

    feat: add teste de falha para criacao de usuario

--- packages/server/src/app/useCases/CreateClient/CreateClient.spec.ts
+++ packages/server/src/app/useCases/CreateClient/CreateClient.spec.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest';
 import InMemoryClientRepository from '../../repositories/in-memory/inMemory.client.repository';
 import CreateClientUseCase from './CreateClientUseCase';
 
-describe('Create Client', () => {
+describe('Create Client - InMemory', () => {
   it('should be able to create a client', async () => {
     const mockClientRepo = new InMemoryClientRepository();
     const useCase = new CreateClientUseCase(mockClientRepo);
@@ -30,5 +30,29 @@ describe('Create Client', () => {
     );
 
     expect(find).toBeTruthy();
+  });
+
+  it('should not be able to create a client with existing email', async () => {
+    const mockClientRepo = new InMemoryClientRepository();
+    const useCase = new CreateClientUseCase(mockClientRepo);
+
+    const mockClientPayload = {
+      name: 'Alexandre',
+      cpf: '06577789566',
+      email: 'alexandreeduardo@email.com',
+      phone: '87992456785',
+      address: {
+        street: 'Rua tal',
+        district: 'Bairro X',
+        city: 'Sao Paulo',
+        state: 'SP',
+        country: 'Brasil',
+        zipcode: '29060670',
+      },
+    };
+
+    await useCase.execute(mockClientPayload);
+
+    expect(useCase.execute(mockClientPayload)).rejects.toThrow();
   });
 });
